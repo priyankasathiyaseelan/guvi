118a6bcf063ee2898c2f17f08d639ea41b4a44b6
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-03-05 21:27:01 -0800

    Create table_9_single_space_781.js

---
+++ javascript/codekata/absolute_beginner/table_9_single_space_781.js
@@ -0,0 +1,8 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  var a=[]
+  for(var i=1;i<=data;i++){
+    a.push(9*i)}
+  console.log(a.join(' '));
+});
