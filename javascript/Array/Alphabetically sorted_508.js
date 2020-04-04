c83bd6ef93367e88a14a4d4aebc331d82122631a
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-04-04 04:52:42 -0700

    Create Alphabetically sorted_508.js

---
+++ javascript/Array/Alphabetically sorted_508.js
@@ -0,0 +1,10 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+const n=[];
+inp.on("line", (data) => {
+    n.push(data);});
+inp.on("close",(data)=>{
+  var a=n[0];
+  var b=n[1].split(" ");
+  console.log(b.sort().join(' '));
+});
