d604e2dd6fb2c95fd572206eed9d0fe59025351f
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-05-06 13:08:41 +0530

    Create map.js

---
+++ javascript/WFM/tasks/map.js
@@ -0,0 +1,29 @@
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+const n=[];
+inp.on('line',(data)=>{
+    n.push(data);
+});
+inp.on('close',()=>{
+    var k=[];
+    var l=0;
+    var sum=0;
+    var res=[];
+    var g;
+inp.on('close',()=>{
+    k=a[0].split(" ");
+function map()
+{
+ for(var i=0;i<k.length;i++)
+ {  
+     l=0;
+     l=parseInt(k[i]*5);
+     //console.log(l);
+     res.push(l);
+ }
+ return g=res.join(" ");
+ 
+}
+res=map();
+console.log(res);
+});
