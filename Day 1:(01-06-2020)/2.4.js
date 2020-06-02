016577099e0be73bbc65b692f980ef1ca568e0aa
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-02 09:32:41 +0530

    Create 2.4.js

---
+++ Day 1:(01-06-2020)/2.4.js
@@ -0,0 +1,42 @@
+//Create a New Object in the library for a new Book called SQL.
+
+function newBookSql(){
+var library=[
+    {
+        title:"javascript",
+        price:500,
+        readers:[
+            {
+                name:"person 1",
+                count:300
+            },
+            {
+                name:"person 2",
+                count:400
+            }
+            ],
+            authordetails:{
+                name:"raj",
+                age:40
+            },
+    },
+    {
+        title:"node js",
+        price:600,
+        readers:[],
+         authordetails:{
+                name:"raj",
+                age:40
+            }
+    }
+    
+    ]
+    
+    //object create
+    
+    var library2 = {titile : 'sql'}
+    }
+    
+    //console.log(library2);
+    }
+    newBookSql();
