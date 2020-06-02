4758f7e62b851efede839303f01f6466719e0cea
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-02 09:45:23 +0530

    Create 2.7.js

---
+++ Day 1:(01-06-2020)/2.7.js
@@ -0,0 +1,41 @@
+  
+//Print the age of Nodejs Author in console.
+
+function AuthorAge(){
+var library=[
+    {
+        title: "javascript",
+        price: 500,
+        readers:[
+            {
+                name: "person 1",
+                count: 300
+            },
+            {
+                name: "person 2",
+                count: 400
+            }
+            ],
+            authordetails:{
+                name: "Raj",
+                age: 40
+            },
+    },
+    {
+        title: "node js",
+        price: 600,
+        readers:[],
+         authordetails:{
+                name: "Raj",
+                age: 40
+            }
+    }
+    ]
+    
+    
+    var age = library[1].authordetails.age;
+    console.log(age);
+    //age is 40
+    }
+    
+    AuthorAge();
