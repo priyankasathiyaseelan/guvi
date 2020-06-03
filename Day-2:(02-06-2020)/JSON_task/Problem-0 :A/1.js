94c8de5c49a517eb978025545d1eefca31eef0fb
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-03 09:10:10 +0530

    Update 1.js

--- Day-2:(02-06-2020)/JSON_task/Problem-0 :A/1.js
+++ Day-2:(02-06-2020)/JSON_task/Problem-0 :A/1.js
@@ -1,1 +1,25 @@
+//Add height and weight to Fluffy
 
+function HW(){
+var cat = {
+ name: "Fluffy",
+ activities: ["play", "eat cat food"],
+ catFriends: [
+ {
+ name: "bar",
+ activities: ["be grumpy", "eat bread omblet"],
+ weight: 8,
+ furcolor: "white"
+ }, 
+ {
+ name: "foo",
+ activities: ["sleep", "pre-sleep naps"],
+ weight: 3
+ }
+ ]
+}
+//add height,weight to fluffy
+var cat[3] = {"height":15,"weight":4};
+console.log(cat[3]);
+}
+HW();
