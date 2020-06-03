8a14470bee42ead3b18c6778b7b41d541ea43e4e
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-03 09:13:26 +0530

    Create 2.js

---
+++ Day-2:(02-06-2020)/JSON_task/Problem-0 :A/2.js
@@ -0,0 +1,25 @@
+//cat name changed :
+
+function NameChange(){
+var cat = {
+ name: "Fluffy",
+ activities: ["play", "eat cat food"],
+ catFriends: [
+ {
+ name: "bar",
+ activities: ["be grumpy", "eat bread omblet"],
+ weight: 8,
+ furcolor: "white"
+ }, 
+ {
+ name: "foo",
+ activities: ["sleep", "pre-sleep naps"],
+ weight: 3
+ }
+ ]
+}
+//
+cat.name = "Fluffy";
+console.log(cat.name);
+}
+NameChange();
