984e4efce2966dbabae47306b905d51a7ee43eea
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-04 17:42:11 +0530

    Update 3.js

--- Day-3:(03-06-2020)/3.js
+++ Day-3:(03-06-2020)/3.js
@@ -11,4 +11,41 @@ function demoFun1(){
 }
 demoFun1(10,demoFun);
 
+//Problem: 3.2 
+//if the given number is prime
+
+function demoFun(num,prime){
+var x=0
+for(var i=2;i<num;i++){
+if(num%i == 0){
+x=1
+break;
+}
+}
+if(x == 0){
+prime()
+}
+}
+function prime(){
+console.log("Yes")
+}
+demoFun(4,prime)
+console.log("No");   //No
+
+//Problem: 3.3
+//if the number is palindrome
+
+function demoFun(num,palindrome){
+var arr=num.split('').reverse().join('')
+if(arr == num){
+palindrome()
+}
+}
+function palindrome(){
+console.log("Yes,it is Palindrome")  //Yes,it is Palindrome
+}
+demoFun('MoM',palindrome)
+
+
+
 
