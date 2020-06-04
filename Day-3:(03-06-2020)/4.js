4f7af285c56f0bf5b0e2093853f194e5ce3802a7
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-04 18:50:38 +0530

    Create 4.js

---
+++ Day-3:(03-06-2020)/4.js
@@ -0,0 +1,22 @@
+//Do the below programs in arrow functions :
+
+//Problem: 4.1
+//Print odd numbers in an array
+
+var array = [1,2,3,4,5];
+var odd = array.filter ((n)=>
+	 n = (n%2 !=0);
+);
+console.log(odd);   //[1,3,5]
+
+//Problem: 4.3
+//Sum of all numbers in an array
+
+var array = [1,2,3,4,5]
+var sum = array.reduce((s1,s2)=>
+	num = s1+s2
+);
+console.log(sum);  //15
+
+
+
