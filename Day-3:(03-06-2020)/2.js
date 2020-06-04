4935740eefae3225443e1c0b25cbcbfc65b08051
priyankasathiyaseelan <52281241+priyankasathiyaseelan@users.noreply.github.com>
2020-06-04 18:41:51 +0530

    Update 2.js

--- Day-3:(03-06-2020)/2.js
+++ Day-3:(03-06-2020)/2.js
@@ -1,6 +1,6 @@
 //Do the below program in an Anonymous function:
 
-//Problem : 1
+//Problem : 2.1
 //Print odd numbers in an array
 var arr = [];
 var odd = function(odd){
@@ -13,7 +13,7 @@ var odd = function(odd){
 }
 odd(9);
 
-//Problem : 2
+//Problem : 2.2
 //Convert all the strings to title caps in a string array 
 
 var demoFun = ["guvi","geek","integra"];
@@ -30,7 +30,7 @@ console.log(arr1);   //["Guvi", "Geek", "Integra"]
 }
 arr(demoFun);
 
-//Problem : 3
+//Problem : 2.3
 //Sum of all numbers in an array
 
 var array = [1,2,3,4,5];
@@ -45,7 +45,7 @@ console.log(count);  //15
 }
 sum(array);  //call funtion
 
-//Problem : 4
+//Problem : 2.4
 //Return all the prime numbers in an array 
 
 var num = 10;
@@ -67,3 +67,34 @@ arr.push(i);   //push prime num into arr
 console.log(arr.join(" "));   //2,3,5,7
 }
 prime(num);
+
+//Problem: 2.5
+//Return all the palindromes in an array
+
+var arr = ['mom','pop','anna']
+var palindrome = arr.filter(function(s){
+  array2 = s.split('').reverse().join('')
+  if(s == array2){
+  return s
+  }
+  });
+  console.log(palindrome);   //["mom", "pop", "anna"]
+
+//Problem: 2.7
+//Remove duplicates from an array
+
+var arr = [1,2,5,4,5]
+var duplicate = function(array){
+    var dup = [...new Set(array)];
+    return dup;
+}
+console.log(duplicate(arr));  //{1,2,5,4]
+
+//Problem: 2.8
+//Rotate an array by k times and return the rotated array
+
+
+
+
+
+
